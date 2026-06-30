a23c93078c1906309c11021718d8828b91cf81ec
gggeek <giunta.gaetano@gmail.com>
2026-06-30 13:08:10 +0000

    cleanup; make one header test non-wildcard

--- src/Matcher/OptionAwareMatcherFactory.php
+++ src/Matcher/OptionAwareMatcherFactory.php
@@ -30,21 +30,8 @@ abstract class OptionAwareMatcherFactory extends SuffixedMatcherFactory
         // remove a suffix such as `:1`, which can be used to obviate to key uniqueness issues
         $typeWithOptions = parent::getMatcherType($type);
         $data = explode($this->optionSeparatorChar, $typeWithOptions);
-        //$out = ['type' => $data[0], 'caseInsensitive' => false, 'expandWildcards' => true];
         $out = $options;
         for ($i = 1; $i < count($data); $i++) {
-            /*switch ($data[$i]) {
-                case 'case_insensitive':
-                case 'insensitive':
-                    $out['caseInsensitive'] = true;
-                    break;
-                case 'exact':
-                case 'exact_match':
-                    $out['expandWildcards'] = false;
-                    break;
-                default:
-                    throw new \Exception("Matcher modifier '/{$data[$i]}' is not supported");
-            }*/
             $optionName = $data[$i];
             if (!array_key_exists($optionName, $options)) {
                 throw new \Exception("Matcher modifier '{$this->optionSeparatorChar}{$optionName}' is not supported");
