2b565d60624e33e126e52f6436f9ed150478bc6b
gggeek <giunta.gaetano@gmail.com>
2026-07-03 21:27:03 +0000

    remove one useless import

--- src/Proxy/Proxy.php
+++ src/Proxy/Proxy.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace YAWAF\Core\Proxy;
 
-use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
