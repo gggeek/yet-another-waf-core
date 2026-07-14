43af80e006b94246a6316275a9815e63a59ca42f
gggeek <giunta.gaetano@gmail.com>
2026-07-14 09:35:10 +0000

    WIP make UnixCompressor compressor work as expected

---
+++ tests/AC_CompressSmokeTest.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace YAWAF\Core\Tests;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use YAWAF\Core\Filter\Bidirectional\UnixCompressor;
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+
+class AC_CompressSmokeTest extends TestCase
+{
+    #[DataProvider('compressTestsDataProvider')]
+    public function testCompressRoundtrip(string $data): void
+    {
+        $compressed = UnixCompressor::compress($data);
+        $decompressed = UnixCompressor::uncompress($compressed);
+
+        $this->assertEquals($data, $decompressed);
+    }
+
+    #[DataProvider('compressTestsDataProvider')]
+    public function testCompressVsNative(string $data): void
+    {
+        $fileName = sys_get_temp_dir() . '/' . md5($data);
+        file_put_contents($fileName, $data);
+        exec("compress -k -f -- " . escapeshellarg($fileName), $out, $retCode);
+
+        //$compressed = UnixCompressor::compress($data);
+        $unixCompressed = file_get_contents($fileName . '.Z');
+/// @todo... enable this, after we fix the trailing padding bytes issue
+        //$this->assertEquals($unixCompressed, $compressed);
+
+        $decompressed = UnixCompressor::uncompress($unixCompressed);
+        $this->assertEquals($data, $decompressed);
+    }
+
+
+    /**
+     * Returns 10 random binary strings for each of lengths range: 1-10, 10-100, 100-1000, 1000-10000, plus ''
+     * @return string[]
+     */
+    public static function compressTestsDataProvider(): array
+    {
+        $out = [['']];
+        $minLen = 1;
+        for ($i = 1; $i < 5; $i++) {
+            $maxLen = 10 ** $i;
+            $len = rand($minLen, $maxLen);
+            for ($j = 0; $j < 10; $j++) {
+                $string = '';
+                for ($k = 1; $k <= $len; $k++) {
+                    $string .= chr(rand(0, 255));
+                }
+                $out[] = [$string];
+            }
+            $minLen = $maxLen;
+        }
+        return $out;
+    }
+}
